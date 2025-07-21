d939183549af057bd537a032c042b581f31fb1a9
Anuradha Belgaonkar <annubelgaonkar@gmail.com>
2025-07-21 21:45:45 +0530

    Creating a new module

---
+++ tutor-session/src/main/java/dev/anuradha/tutorsession/TutorSessionApplication.java
@@ -0,0 +1,13 @@
+package dev.anuradha.tutorsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSessionApplication.class, args);
+    }
+
+}
